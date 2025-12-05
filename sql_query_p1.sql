308a3e4a3f3cdcf0fdfb55e3812a5b64b20258ba
Aryansh Jain <100217693+Aryansh1412@users.noreply.github.com>
2025-12-05 19:02:48 +0530

    Fix column names in retail_sales table
    Corrected column names in retail_sales table definition.

--- sql_query_p1.sql
+++ sql_query_p1.sql
@@ -6,24 +6,22 @@ CREATE DATABASE sql_project_p2;
 DROP TABLE IF EXISTS retail_sales;
 CREATE TABLE retail_sales
             (
-                transaction_id INT PRIMARY KEY,	
+                transactions_id INT PRIMARY KEY,	
                 sale_date DATE,	 
                 sale_time TIME,	
-                customer_id	INT,
+                customer_id INT,
                 gender	VARCHAR(15),
-                age	INT,
+                age INT,
                 category VARCHAR(15),	
-                quantity	INT,
+                quantiy INT,
                 price_per_unit FLOAT,	
-                cogs	FLOAT,
+                cogs FLOAT,
                 total_sale FLOAT
             );
 
 SELECT * FROM retail_sales
 LIMIT 10
 
-
-    
 
 SELECT 
     COUNT(*) 
